05d0f301755bff1d957912e15ec0cdf6e1d8b9b0
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-23 23:23:01 +0530

    added required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div>HELLO!</div>
+    <div className="bg-purple-800 min-h-screen px-6 py-3 flex flex-col gap-16">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
   )
 }
 
