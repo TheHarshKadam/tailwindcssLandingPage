5bb265f49491e590c787a35d051eae351f40e700
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-23 23:25:12 +0530

    Added boiler plate

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src="./src/assets/Logo.svg" alt="logo" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
