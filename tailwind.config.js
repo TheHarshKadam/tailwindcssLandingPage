1bb6bedffa528141a5c2815ce33a4212f03645d6
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-25 00:58:15 +0530

    Added custom CSS and Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        trispace : '"Trispace", sans-serif;',
+        natoSansKorean: '"Noto Sans KR", sans-serif;',
+        poppins : '"Poppins", sans-serif;'
+      },
+      colors :{
+        'myIndigo' :'#372b5e',
+        'myOrange' : '#ff646c'
+      }
+    },
   },
   plugins: [],
 }
