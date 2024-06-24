9050418b68270c7b5722a0de7d04df2da3cd9c01
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-25 01:16:33 +0530

    added body components and desgin

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+      <div>
+        <img src="./src/assets/bodyImg.png" alt="bodyImage" />
+      </div>
+      <div>
+        <h1 className='text-5xl font-natoSansKorean font-bold leading-tight text-slate-200 pb-4'>Enjoying Space Like Never Before</h1>
+        <p className='text-xl text-slate-200 font-poppins pb-5'>
+          Explore the Universe with interactives and Videos galaxies, 
+          and Universe, giving opportunity to experience space and Earth as seen with satellites
+        </p>
+        <button className='bg-myOrange'>Join Us!</button>
+      </div>
+    </div>
   )
 }
 
