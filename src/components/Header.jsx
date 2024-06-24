c26281d88ddd004383fe86b547140d558f6617af
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-25 01:15:49 +0530

    added header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex justify-between'>
+        <div className='flex items-center justify-center gap-3'>
+            <img src="./src/assets/logo.png" alt="logo" style={{ width: '35px', height: 'auto' }} />
+            <button className='text-slate-200 text-xl font-bold font-trispace'>Space Spark!</button>
+        </div>
         <div>
-            <img src="./src/assets/Logo.svg" alt="logo" />
+          <FaBars size={35} className='text-slate-200'/>
         </div>
     </div>
   )
