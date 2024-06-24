f9badce2485b10a9d44f78059b96d2bb2b2137b5
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-06-25 01:14:04 +0530

    changed background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="bg-purple-800 min-h-screen px-6 py-3 flex flex-col gap-16">
+    <div className="bg-myIndigo min-h-screen px-6 py-3 flex flex-col gap-16">
       <Header/>
       <Body/>
       <Footer/>
