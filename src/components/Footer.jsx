e3205ac9cd49a455ef2644d87d27e1d83dde2676
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-07-02 00:42:16 +0530

    added responsiveness to webpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='hidden lg:block'>Footer</div>
+    <div className='hidden lg:flex lg: justify-center items-center h-12 mt-auto font-poppins text-slate-200'>Made With Love By THK ❤ </div>
   )
 }
 
