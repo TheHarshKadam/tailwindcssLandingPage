46a6fe52675db8257085a95491984eb3748c71d3
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2024-07-02 00:42:55 +0530

    added responsiveness to webpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,15 @@ const Header = () => {
             <img src="./src/assets/logo.png" alt="logo" style={{ width: '35px', height: 'auto' }} />
             <button className='text-slate-200 text-xl font-bold font-trispace'>Space Spark!</button>
         </div>
-        <div>
+        <ul className='hidden lg:flex lg:ml-auto gap-6 lg:py-2 text-slate-200 text-xl font-bold font-trispace'>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Community</a></li>
+        </ul>
+        <div className='hidden lg:block ml-6 '>
+        <button className='bg-myOrange px-5 py-2 text-lg font-poppins text-white rounded-full'>Join Us!</button>
+        </div>
+        <div className='lg:hidden'>
           <FaBars size={35} className='text-slate-200'/>
         </div>
     </div>
